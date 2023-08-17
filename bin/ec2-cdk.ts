2f1079c133a1be0510e146ef39b8603dc3852de0
Abdirahim Hussein <abdirahimhussein@Abdirahims-MacBook-Air.local>
2023-08-17 11:43:40 +0100

    finalcommit

--- bin/ec2-cdk.ts
+++ bin/ec2-cdk.ts
@@ -11,11 +11,11 @@ new PythonEc2BlogpostStack(app, 'Ec2CdkStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '1571-5870-2269', region: 'eu-west-1' },
 
   
 });
