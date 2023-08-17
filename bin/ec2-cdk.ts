92b879528e0aaaece2394995beaace4ef44bacb0
Abdirahim Hussein <abdirahimhussein@Abdirahims-MacBook-Air.local>
2023-08-17 11:37:25 +0100

    finalcommit

--- bin/ec2-cdk.ts
+++ bin/ec2-cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { Ec2CdkStack } from '../lib/ec2-cdk-stack';
+import { PythonEc2BlogpostStack } from '../lib/ec2-cdk-stack';
 
 const app = new cdk.App();
-new Ec2CdkStack(app, 'Ec2CdkStack', {
+new PythonEc2BlogpostStack(app, 'Ec2CdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
